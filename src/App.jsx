87020d66e349dc732b969d9375bb29ad42d32127
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-04-30 11:46:50 +0430

    🚀 chore(package.json): add notistack dependency to project ✨ feat(QuizQuestion.jsx): add snackbar notification when quiz ends 🎨 refactor(QuizQuestion.jsx): remove NavBar from QuizQuestion component and add it conditionally 🎨 refactor(main.jsx): wrap App component with SnackbarProvider to use notistack notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import NavBar from './components/NavBar/NavBar';
 import QuizQuestion from './components/QuizQuestion/QuizQuestion';
 
 function App() {
   return (
     <div className='App w-full h-screen flex flex-col items-center justify-center bg-blue-300'>
-      <NavBar />
       <QuizQuestion />
     </div>
   );
--- src/components/QuizQuestion/QuizQuestion.jsx
+++ src/components/QuizQuestion/QuizQuestion.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
+import { useSnackbar } from 'notistack';
 import * as data from '../../../data/questions';
+import NavBar from '../NavBar/NavBar';
 
 const QuizQuestion = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
   const questions = data.questions;
+  const { enqueueSnackbar } = useSnackbar();
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [isShowScore, setIsShowScore] = useState(false);
+  const [scoreValue, setScoreValue] = useState(0);
 
   const getRandomized = (array) => {
     const randomizedOptions = [...array];
@@ -18,7 +23,6 @@ const QuizQuestion = () => {
   };
 
   const shuffledQuestions = getRandomized(questions);
-  console.log(shuffledQuestions);
 
   const shuffledAnswerOptions = getRandomized(
     questions[currentQuestion].answerOptions
@@ -29,45 +33,69 @@ const QuizQuestion = () => {
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
-      alert('End of Quiz');
+      enqueueSnackbar('End of the quiz !', {
+        variant: 'default',
+        autoHideDuration: 3000,
+        preventDuplicate: true,
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'center',
+        },
+      });
     }
   };
 
-  // console.log(shuffledAnswerOptions);
-
   return (
-    <div className='flex flex-col bg-white max-w-xl w-full  rounded-b-2xl shadow-xl px-10 py-6'>
-      <div className='flex justify-start gap-x-1 font-semibold text-lg text-slate-800 mb-4 '>
-        <span>{currentQuestion + 1}.</span>
-        <p>{shuffledQuestions[currentQuestion].questionText}</p>
+    <>
+      {!isShowScore && <NavBar />}
+      <div
+        className={
+          isShowScore
+            ? 'flex flex-col bg-white max-w-xl w-full rounded-2xl shadow-xl px-10 py-6 h-20'
+            : 'flex flex-col bg-white max-w-xl w-full rounded-b-2xl shadow-xl px-10 py-6'
+        }
+      >
+        {isShowScore ? (
+          <div className='text-center font-bold text-xl'>
+            You scored {scoreValue} of {questions.length} (
+            {(scoreValue / questions.length) * 100} %)
+          </div>
+        ) : (
+          <>
+            <div className='flex justify-start gap-x-1 font-semibold text-lg text-slate-800 mb-4 '>
+              <span>{currentQuestion + 1}.</span>
+              <p>{shuffledQuestions[currentQuestion].questionText}</p>
+            </div>
+            <div className='flex flex-col py-2 gap-y-4'>
+              {shuffledAnswerOptions.map((answerOption) => {
+                return (
+                  <button
+                    key={crypto.randomUUID()}
+                    className='bg-blue-100 hover:bg-blue-200 hover:border-blue-300 border-2 border-blue-200 rounded-md w-full px-4 py-2 text-lg font-medium text-slate-800 text-left'
+                  >
+                    {answerOption.answerText}
+                  </button>
+                );
+              })}
+            </div>
+            <hr className='border-slate-800 mt-2' />
+            <div className='flex items-center justify-between mt-4'>
+              <span className='font-semibold text-lg text-slate-800'>
+                {currentQuestion + 1} of {questions.length} Questions
+              </span>
+              <button
+                onClick={answerButtonHandler}
+                className='bg-blue-400 px-4 py-2 rounded-md text-white text-base hover:bg-blue-600'
+              >
+                {currentQuestion + 1 === questions.length
+                  ? 'Finish'
+                  : 'Next Question'}
+              </button>
+            </div>
+          </>
+        )}
       </div>
-      <div className='flex flex-col py-2 gap-y-4'>
-        {shuffledAnswerOptions.map((answerOption) => {
-          return (
-            <button
-              key={crypto.randomUUID()}
-              className='bg-blue-100 hover:bg-blue-200 hover:border-blue-300 border-2 border-blue-200 rounded-md w-full px-4 py-2 text-lg font-medium text-slate-800 text-left'
-            >
-              {answerOption.answerText}
-            </button>
-          );
-        })}
-      </div>
-      <hr className='border-slate-800 mt-2' />
-      <div className='flex items-center justify-between mt-4'>
-        <span className='font-semibold text-lg text-slate-800'>
-          {currentQuestion + 1} of {questions.length} Questions
-        </span>
-        <button
-          onClick={answerButtonHandler}
-          className='bg-blue-400 px-4 py-2 rounded-md text-white text-base hover:bg-blue-600'
-        >
-          {currentQuestion + 1 === questions.length
-            ? 'Finish'
-            : 'Next Question'}
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
