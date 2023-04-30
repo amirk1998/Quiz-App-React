42bbede95c2df55ba0cd02ea1ab3bce1a6fa1c1c
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-04-30 10:07:59 +0430

    Starter Project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='App w-full h-screen flex flex-col items-center bg-gray-100'>
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuizQuestion/QuizQuestion.jsx
@@ -0,0 +1,5 @@
+const QuizQuestion = () => {
+  return <div className='flex flex-col items-center '></div>;
+};
+
+export default QuizQuestion;
